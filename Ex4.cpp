afae9ef615ecdcadb635c519b28d2fde73bd6e45
BiKend <bikend.learn@gmail.com>
2024-05-18 13:28:36 +0400

    Изменения: Добавлен метод returnBook в класс TBook для возврата книги. Создан класс Library для управления коллекцией книг. Добавлен метод addBook в класс Library для добавления книги в библиотеку. Добавлены методы findBookByTitle и findBookByAuthor для поиска книги по названию и автору соответственно. Добавлен метод displayAllBooks для отображения информации обо всех книгах в библиотеке. Обновлен метод main для демонстрации работы библиотеки и операций с книгами.

--- Ex4.cpp
+++ Ex4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 // Класс Книга
 class TBook {
@@ -20,20 +21,19 @@ public:
   }
 
   // Метод для получения названия книги
-  std::string getTitle() {
+  std::string getTitle() const {
     return title;
   }
 
   // Метод для получения автора книги
-  std::string getAuthor() {
+  std::string getAuthor() const {
     return author;
   }
 
   // Метод для получения количества экземпляров книги
-  int getCount() {
+  int getCount() const {
     return count;
   }
-<<<<<<< HEAD
 
   // Метод для установки нового значения количества экземпляров книги
   void setCount(int newCount) {
@@ -52,28 +52,100 @@ public:
     std::cout << "Автор: " << author << std::endl;
     std::cout << "Количество экземпляров: " << count << std::endl;
   }
-=======
->>>>>>> 872c37a9258342421ee70bf6cd17f0add91fcb05
+
+  // Метод для проверки наличия экземпляров книги
+  bool isAvailable() const {
+    return count > 0;
+  }
+
+  // Метод для выдачи книги (уменьшение количества экземпляров)
+  bool borrowBook() {
+    if (isAvailable()) {
+      count--;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // Метод для возврата книги (увеличение количества экземпляров)
+  void returnBook() {
+    count++;
+  }
+};
+
+// Класс Библиотека
+class Library {
+private:
+  std::vector<TBook> books; // Коллекция книг
+
+public:
+  // Метод для добавления книги в библиотеку
+  void addBook(const TBook& book) {
+    books.push_back(book);
+  }
+
+  // Метод для поиска книги по названию
+  TBook* findBookByTitle(const std::string& title) {
+    for (auto& book : books) {
+      if (book.getTitle() == title) {
+        return &book;
+      }
+    }
+    return nullptr;
+  }
+
+  // Метод для поиска книги по автору
+  TBook* findBookByAuthor(const std::string& author) {
+    for (auto& book : books) {
+      if (book.getAuthor() == author) {
+        return &book;
+      }
+    }
+    return nullptr;
+  }
+
+  // Метод для отображения информации обо всех книгах
+  void displayAllBooks() const {
+    for (const auto& book : books) {
+      book.displayInfo();
+      std::cout << std::endl;
+    }
+  }
 };
 
 int main() {
-  // Создаем объект книги
-  TBook book("Программирование на C++", "Иван Иванов", 5);
+  // Создаем объект библиотеки
+  Library library;
 
-  // Выводим информацию о книге
-  book.displayInfo();
+  // Добавляем книги в библиотеку
+  library.addBook(TBook("Программирование на C++", "Иван Иванов", 5));
+  library.addBook(TBook("Продвинутое программирование на C++", "Петр Петров", 3));
 
-<<<<<<< HEAD
-  // Изменяем количество экземпляров книги
-  book.setCount(7);
+  // Выводим информацию обо всех книгах в библиотеке
+  std::cout << "Все книги в библиотеке:\n";
+  library.displayAllBooks();
 
-  // Изменяем название и автора книги
-  book.setDetails("Продвинутое программирование на C++", "Петр Петров");
+  // Поиск книги по названию и выдача книги
+  TBook* book = library.findBookByTitle("Программирование на C++");
+  if (book != nullptr) {
+    std::cout << "\nКнига найдена:\n";
+    book->displayInfo();
+    if (book->borrowBook()) {
+      std::cout << "Книга успешно выдана.\n";
+    } else {
+      std::cout << "Книга недоступна для выдачи.\n";
+    }
+  } else {
+    std::cout << "Книга не найдена.\n";
+  }
 
-  // Выводим обновленную информацию о книге
-  book.displayInfo();
+  // Возврат книги
+  if (book != nullptr) {
+    book->returnBook();
+    std::cout << "\nКнига возвращена:\n";
+    book->displayInfo();
+  }
 
-=======
->>>>>>> 872c37a9258342421ee70bf6cd17f0add91fcb05
   return 0;
 }
